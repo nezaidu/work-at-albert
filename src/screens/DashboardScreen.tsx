e3be477364dd098ade960260f65037d9040215a4
Andrei Sorocinschi <nezaidu@gmail.com>
2021-09-27 16:08:18 +0300

    use RNN navigation

---
+++ src/screens/DashboardScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+
+
+
+const DashboardScreen = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: 'wheat' }}>
+
+    </View>
+  )
+};
+
+
+export default DashboardScreen;
